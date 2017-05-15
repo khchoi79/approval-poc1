997b397591eff0813c94ae325bb0e530eb8b6065
Jin Gi Kong <jgkong@kr.ibm.com>
2017-05-15 03:56:58 +0000

    Gate API prototype
    * Implement sdevopsadm/roadmap#23

--- server.js
+++ server.js
@@ -10,5 +10,31 @@ app.get("/sayHello", function (request, response) {
   response.end("Hello " + user_name + "!");
 });
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+var approvals={}
+
+app.post('/api/v1/toolchain/:toolchain_id/:pipeline_id', function(req, res) {
+  var data = {
+    'toolchain_id': req.params.toolchain_id,
+    'pipeline_id': req.params.pipeline_id,
+    'input_rev': req.params.input_rev,
+    'body': req.body
+  }
+  console.log(data);
+  var input_rev = req.params.input_rev;
+  var result = {};
+  if (input_rev in approvals) {
+    result['status'] = approvals[input_rev];
+  } else {
+    // create new approval record
+    approvals[input_rev] = 'pending';
+    result['status'] = 'created';
+  }
+  res.send(result);
+});
+
 app.listen(port);
 console.log("Listening on port ", port);
