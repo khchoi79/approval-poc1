eda5d128f5c051b381937618e878a6c110dc1fa4
Jin Gi Kong <jgkong@kr.ibm.com>
2017-06-13 13:55:53 +0000

    Fix conenction problem with Compose for MongoDB
    * MongoError: no primary found in replicaset

--- databases/mongodb.js
+++ databases/mongodb.js
@@ -3,6 +3,11 @@ var log = require('../utils/logger')('mongoose')
 module.exports = function (url, dbName) {
   let mongoose = require('mongoose')
 
+  // temp workaround for mongoose and Compose for MongoDB
+  if (url.indexOf('dblayer.com') > 0 && url.indexOf(',') > 0) {
+    url = url.replace(/,.*\//, '/')
+  }
+
   mongoose.connect(url)
   .then(res => log.info('Database connected'))
   .catch(err => log.error('Failed to connect database', err))
